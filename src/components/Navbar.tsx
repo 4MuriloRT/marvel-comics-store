27a5f92e81001d3a3a4065b8915532bd4f99835c
Murilo Taborda <murilotaborda09@gmail.com>
2025-08-07 12:21:36 -0300

    primeiro commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { FunctionComponent } from "react";
+import styled from "styled-components";
+
+const NavWrapper = styled.nav`
+    display: flex,
+    justify-content: space-between;
+    aling-items: center;
+    background: #282c34;
+    padding: 1rem 2rem;
+    color: white;
+    box-shadow: 0 2px 4px rgba(0, 0, 0.1);
+`;
+
+const Logo = styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+  margin: 0;
+`;
+
+interface NavbarProps {
+  name: string;
+}
+
+const Navbar: FunctionComponent<NavbarProps> = ({ name }) => {
+  return (
+    <NavWrapper>
+      <Logo>{name}</Logo>
+    </NavWrapper>
+  );
+};
+
+export default Navbar;
